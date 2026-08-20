b74c18e0fbb03ac19b0798d911931eb0d7fbf57e
Jonathan Salazar <salazarbarrrerajonathan99@gmail.com>
2026-08-19 23:12:58 -0600

    Agregar CI con GitHub Actions y modo headless

--- conftest.py
+++ conftest.py
@@ -1,8 +1,17 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import os
 
 @pytest.fixture
 def driver():
-    nav = webdriver.Firefox()
+    opciones = Options()
+
+    # Si la variable de entorno CI existe (GitHub la crea automáticamente),
+    # corremos Firefox sin ventana visible
+    if os.environ.get("CI"):
+        opciones.add_argument("--headless")
+
+    nav = webdriver.Firefox(options=opciones)
     yield nav
     nav.quit()
